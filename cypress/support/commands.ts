58093530efa5e1da4ef190ec0a6236cdc5564f0d
julio cesar <jcmcf2012@gmail.com>
2023-07-29 09:36:36 -0300

    infra: add commands to store the local storage between tests

---
+++ cypress/support/commands.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+// ***********************************************
+
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("mSaveLocalStorage", () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add("mRestoreLocalStorage", () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
